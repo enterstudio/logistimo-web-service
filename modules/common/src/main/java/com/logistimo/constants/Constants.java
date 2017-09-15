3ea13e806f6ea82aada3832fe6efd220466f9fbd
kgaurav1 <kumar@logistimo.com>
2017-09-15 15:39:23 +0530

    changes done to fix user export issue (for app version text) (#422)
    * changes done to fix user export issue (for app version dependency)
    
    * changes done to fix user export issue (for app version dependency)
    
    * Changed the resource bundle for Iota

--- modules/common/src/main/java/com/logistimo/constants/Constants.java
+++ modules/common/src/main/java/com/logistimo/constants/Constants.java
@@ -219,7 +219,6 @@ public final class Constants {
   public static final String APP_VER = "x-app-ver";
   public static final String MMA_NAME = "mma";
 
-
   public static final int FREQ_TYPE_DAILY = 0;
   public static final int FREQ_TYPE_WEEKLY = 1;
   public static final int FREQ_TYPE_MONTHLY = 2;
